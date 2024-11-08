defa9fd517b6c67ce9938f0b74fe0e1a9f3497f7
Muhammad Bilal Khan <bilalkhan0026@>
2024-11-08 01:05:44 -0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+const nextConfig = {
+    reactStrictMode: true, // Yeh React strict mode ko enable karta hai
+    swcMinify: true,       // Yeh SWC minification ko enable karta hai
+  }
+  
+  module.exports = nextConfig;
+  
